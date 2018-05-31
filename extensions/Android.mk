ded583348d43aa03ea0275d9b89eafa78140c42e
yzqhao <yzqhao@163.com>
2018-05-31 10:45:32 +0800

    移除scrollview和无用资源

--- extensions/Android.mk
+++ extensions/Android.mk
@@ -7,10 +7,7 @@ LOCAL_MODULE_FILENAME := libextension
 
 LOCAL_ARM_MODE := arm
 
-LOCAL_SRC_FILES := \
-GUI/CCScrollView/CCScrollView.cpp \
-GUI/CCScrollView/CCTableView.cpp \
-GUI/CCScrollView/CCTableViewCell.cpp 
+LOCAL_SRC_FILES := 
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_internal_static
 
@@ -19,7 +16,6 @@ LOCAL_CXXFLAGS += -fexceptions
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/. \
-                           $(LOCAL_PATH)/.. \
-                           $(LOCAL_PATH)/GUI/CCScrollView 
+                           $(LOCAL_PATH)/.. 
                     
 include $(BUILD_STATIC_LIBRARY)
--- extensions/cocos-ext.h
+++ extensions/cocos-ext.h
@@ -3,7 +3,4 @@
 
 #include "ExtensionMacros.h"
 
-#include "GUI/CCScrollView/CCScrollView.h"
-#include "GUI/CCScrollView/CCTableView.h"
-
 #endif /* __COCOS2D_EXT_H__ */
